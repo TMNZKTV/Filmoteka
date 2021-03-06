fcf53ff867cb57f9287d2c3abf26d565fb3f831a
smolfly <68849295+smolfly@users.noreply.github.com>
2021-03-06 13:12:17 +0300

    Маркап шаблона галереи

---
+++ src/js/gallery-markup.js
@@ -0,0 +1,13 @@
+import itemsTemplate from '../templates/movieGallery.hbs'
+import refs from "./refs";
+const {galleryRef} = refs;
+
+function getMarkupGallery () {
+    const markup = itemsTemplate();
+    galleryRef.insertAdjacentHTML('beforeend', markup);
+   
+}
+
+getMarkupGallery();
+
+export default getMarkupGallery;
