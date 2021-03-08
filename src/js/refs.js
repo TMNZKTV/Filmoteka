7f5a71c4e8d526560c0f2d79d86b72032dc446c3
Axeliriya <axeliriya@mail.ru>
2021-03-08 23:19:07 +0200

    добавила клик по страницам в библиотеке

--- src/js/refs.js
+++ src/js/refs.js
@@ -19,13 +19,16 @@ const refs = {
   toNextPageBtn: document.querySelector(
     '.js-pagination .pagination__arrow-right',
   ),
+  toPrevPageBtnLibrery: document.querySelector('.library-pagination__arrow-left'),
+  toNextPageBtnLibrery: document.querySelector(
+    '.library-pagination__arrow-right',
+  ),
   paginationList: document.querySelector('.js-pagination .pagination__list'),
 
   homePagination: document.querySelector('.js-pagination'),
   libraryPaginationList: document.querySelector('.library-pagination__list'),
   libraryPagination: document.querySelector('.library-pagination'),
   headerBack: document.querySelector('.page__header'),
-
 };
 
 export default refs;
--- src/js/showLibrery.js
+++ src/js/showLibrery.js
@@ -4,23 +4,35 @@ import { changeColorBtn } from './librery';
 import apiService from './apiService';
 import getMarkupGallery from './gallery-markup.js';
 
-const { galleryLibrery, libraryPagination, btnWatched, btnQueue } = refs;
+const {
+  galleryLibrery,
+  libraryPagination,
+  btnWatched,
+  btnQueue,
+  toPrevPageBtnLibrery,
+  toNextPageBtnLibrery,
+} = refs;
+
+let page = 1;
 
 function showWatched() {
   changeColorBtn(btnWatched, btnQueue);
   galleryLibrery.innerHTML = '';
   if (idWatched.length > 0) {
-    idWatched.forEach(id => {
-      apiService
-        .fetchID(id)
-        .then(array => getMarkupGallery([array], galleryLibrery));
-    });
+    if (idWatched.length > 19) {
+      libraryPagination.classList.remove('is-hidden');
+      page = 1;
+      getListByPage(page, idWatched).forEach(id => markupListByPage(id));
+
+      toNextPageBtnLibrery.addEventListener('click', onNextPageWatched);
+      toPrevPageBtnLibrery.addEventListener('click', onPrevPageWatched);
+    } else {
+      libraryPagination.classList.add('is-hidden');
+      idWatched.forEach(id => markupListByPage(id));
+    }
   } else {
+    libraryPagination.classList.add('is-hidden');
     galleryLibrery.innerHTML = `<li class="modal__keyItem"><p style="color:red;font-size:24px;text-align:center;">Вы ничего не добавили</p></li>`;
-  }
-  libraryPagination.classList.add('is-hidden');
-  if (idWatched.length > 19) {
-    libraryPagination.classList.remove('is-hidden');
   }
 }
 
@@ -28,20 +40,76 @@ function showQueue() {
   changeColorBtn(btnQueue, btnWatched);
   galleryLibrery.innerHTML = '';
   if (idQueue.length > 0) {
-    idQueue.forEach(id => {
-      apiService
-        .fetchID(id)
-        .then(array => getMarkupGallery([array], galleryLibrery));
-    });
+    if (idQueue.length > 19) {
+      libraryPagination.classList.remove('is-hidden');
+      getListByPage(page, idQueue).forEach(id => markupListByPage(id));
+
+      // здесь нужны кнопки для queue, иначе срабатывают клики watched
+
+      //   toNextPageBtnLibrery.addEventListener('click', onNextPageQueue);
+      //   toPrevPageBtnLibrery.addEventListener('click', onPrevPageQueue);
+    } else {
+      libraryPagination.classList.add('is-hidden');
+      idQueue.forEach(id => markupListByPage(id));
+    }
   } else {
+    libraryPagination.classList.add('is-hidden');
     galleryLibrery.innerHTML = `<li class="modal__keyItem"><p style="color:red;font-size:24px;text-align:center;">Вы ничего не добавили</p></li>`;
-  }
-  libraryPagination.classList.add('is-hidden');
-  if (idQueue.length > 19) {
-    libraryPagination.classList.remove('is-hidden');
   }
 }
 
-// function perPage(page) {}
+function markupListByPage(id) {
+  apiService
+    .fetchID(id)
+    .then(array => getMarkupGallery([array], galleryLibrery));
+}
+
+function onNextPageWatched() {
+  galleryLibrery.innerHTML = '';
+  page += 1;
+  getListByPage(page, idWatched).forEach(id => markupListByPage(id));
+  if (getListByPage(page, idWatched).length < 1) {
+    galleryLibrery.innerHTML = `<li class="modal__keyItem"><p style="color:red;font-size:24px;text-align:center;">Вернитесь назад</p></li>`;
+  }
+}
+
+function onPrevPageWatched() {
+  galleryLibrery.innerHTML = '';
+  page -= 1;
+  getListByPage(page, idWatched).forEach(id => markupListByPage(id));
+  if (getListByPage(page, idWatched).length < 1) {
+    galleryLibrery.innerHTML = `<li class="modal__keyItem"><p style="color:red;font-size:24px;text-align:center;">Ткните вперед</p></li>`;
+  }
+}
+
+function onNextPageQueue() {
+  galleryLibrery.innerHTML = '';
+  page += 1;
+  getListByPage(page, idQueue).forEach(id => markupListByPage(id));
+  if (getListByPage(page, idQueue).length < 1) {
+    galleryLibrery.innerHTML = `<li class="modal__keyItem"><p style="color:red;font-size:24px;text-align:center;">Вернитесь назад</p></li>`;
+  }
+}
+
+function onPrevPageQueue() {
+  galleryLibrery.innerHTML = '';
+  page -= 1;
+  getListByPage(page, idQueue).forEach(id => markupListByPage(id));
+  if (getListByPage(page, idQueue).length < 1) {
+    galleryLibrery.innerHTML = `<li class="modal__keyItem"><p style="color:red;font-size:24px;text-align:center;">Ткните вперед</p></li>`;
+  }
+}
+
+//функция, в которую приходит номер страницы и массив из локал-стореджа
+// результат функции - масиив длинной 20 (одна страница)
+
+function getListByPage(pageNumber, fullList) {
+  if (pageNumber < 1) return [];
+
+  const itemsPerPage = 20;
+  const start = (pageNumber - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  return fullList.slice(start, end);
+}
 
 export { showWatched, showQueue };
