bddad7af9c5dacd29d6212efa9ffa5f5310b2eee
AnastasiaPolkovnikova <68498874+AnastasiaPolkovnikova@users.noreply.github.com>
2021-03-09 04:34:51 +0800

    добавляет стили для нумерации страниц

--- src/js/fetchService.js
+++ src/js/fetchService.js
@@ -16,85 +16,136 @@ refs.paginationList.addEventListener('click', onPageClick);
 function initialFetch() {
   cleanMarkup();
   cleanPagesMarkup(); 
-  let itemList = [];
+
   apiService.fetchPopularMovies().then(({ results, page, total_pages }) => {
     getMarkupGallery(results);
-    if (total_pages - page < 7) {
-      for (let i = total_pages - 1; i > total_pages - 7; i -= 1) {
+    apiService.page = page;
+    apiService.setMaxPage(total_pages);
+    let listToShow = '';
+    function numberMarkup() {
+      let listItems = [];
+      const hiddenPrevPages = '<li class="pagination__item"><button type="button" data-action="showPrevPages">...</button></li>';
+      const hiddenNextPages = '<li class="pagination__item"><button type="button" data-action="showNextPages">...</button></li>';
+      if (total_pages - page < 6) {
+        for (let i = total_pages; i > total_pages - 6; i -= 1) {
+          const item = `<li class="pagination__item"><button type="button">${i}</button></li>`;
+          listItems.unshift(item);
+        } 
+      } else {
+      for (let i = page; i < page + 6 && i < total_pages; i += 1) {
         const item = `<li class="pagination__item"><button type="button">${i}</button></li>`;
-        itemList.unshift(item);
-      } 
-    } else {
-      for (let i = page; i < page + 7 && i < total_pages; i += 1) {
-        const item = `<li class="pagination__item"><button type="button">${i}</button></li>`;
-        itemList.push(item);
+        listItems.push(item);
+      }}
+    
+      listToShow = listItems.join(' ');
+      if (total_pages > 6) {
+        listToShow = listItems.join(' ') + hiddenNextPages + `<li class="pagination__item"><button type="button">${total_pages}</button></li>`;
+      }
+      if (page > 5) {
+        listToShow = `<li class="pagination__item"><button type="button">1</button></li>` + hiddenPrevPages + listItems.join(' ') + hiddenNextPages + `<li class="pagination__item"><button type="button">${total_pages}</button></li>`;
+      }
+      if (total_pages > 6 && page > total_pages - 6 ) {
+        listToShow = `<li class="pagination__item"><button type="button">1</button></li>` + hiddenPrevPages + listItems.join(' ');
       }
     }
-    let listToShow = itemList.join(' ') + `<li class="pagination__item"><button type="button">${total_pages}</button></li>`;
-    refs.paginationList.insertAdjacentHTML('beforeend', listToShow);
+    numberMarkup();
+    refs.paginationList.insertAdjacentHTML('beforeend', listToShow); 
+    highlightCurrentPage();
   })
 }
 
 function fetchByKeyWords() {
   cleanMarkup();
   cleanPagesMarkup(); 
-  let itemList = [];
   
   apiService.fetchMovies().then(({ results, page, total_pages }) => {
     getMarkupGallery(results);
+    apiService.page = page;
+    apiService.setMaxPage(total_pages);
+
     if (results.length === 0) {
       notifyInfo('Try another word', 'No images found for this request');
     }
     
-    if (total_pages - page < 7) {
-      for (let i = total_pages - 1; i > total_pages - 7; i -= 1) {
+    let listToShow = '';
+    function numberMarkup() {
+      let listItems = [];
+      const hiddenPrevPages = '<li class="pagination__item"><button type="button" data-action="showPrevPages">...</button></li>';
+      const hiddenNextPages = '<li class="pagination__item"><button type="button" data-action="showNextPages">...</button></li>';
+      if (total_pages - page < 6) {
+        for (let i = total_pages; i > total_pages - 6; i -= 1) {
+          const item = `<li class="pagination__item"><button type="button">${i}</button></li>`;
+          listItems.unshift(item);
+        } 
+      } else {
+      for (let i = page; i < page + 6 && i < total_pages; i += 1) {
         const item = `<li class="pagination__item"><button type="button">${i}</button></li>`;
-        itemList.unshift(item);
-      } 
-    } else {
-      for (let i = page; i < page + 7 && i < total_pages; i += 1) {
-        const item = `<li class="pagination__item"><button type="button">${i}</button></li>`;
-        itemList.push(item);
+        listItems.push(item);
+      }}
+      
+      listToShow = listItems.join(' ') + `<li class="pagination__item"><button type="button">${total_pages}</button></li>`;
+      if (total_pages > 6) {
+        listToShow = listItems.join(' ') + hiddenNextPages + `<li class="pagination__item"><button type="button">${total_pages}</button></li>`;
+      }
+      if (page > 2) {
+        listToShow = `<li class="pagination__item"><button type="button">1</button></li>` + hiddenPrevPages + listItems.join(' ') + hiddenNextPages + `<li class="pagination__item"><button type="button">${total_pages}</button></li>`;
+      }
+      if (total_pages > 6 && page > total_pages - 6 ) {
+        listToShow = `<li class="pagination__item"><button type="button">1</button></li>` + hiddenPrevPages + listItems.join(' ');
       }
     }
-
-    let listToShow = itemList.join(' ') + `<li class="pagination__item"><button type="button">${total_pages}</button></li>`;
+    numberMarkup();
     refs.paginationList.insertAdjacentHTML('beforeend', listToShow);
-
+    
+    highlightCurrentPage();
   });
 }
 
 function onInputSearch(event) {
   const movieToFind = event.target.value;
   apiService.query = movieToFind;
-  apiService.resetPage();
-  fetchByKeyWords();
-
   if (apiService.query === '') {
     apiService.resetPage();
     cleanMarkup();
     initialFetch();
+    return;
   }
+  apiService.resetPage();
+  fetchByKeyWords();
 }
 
 function onPageClick(event) {
-  apiService.page = event.target.innerHTML;
+  if (event.target.nodeName !== 'BUTTON') {
+    return;
+  }
+  if (event.target.dataset.action === 'showNextPages') {
+    apiService.page += 6; 
+    makePopularOrKeyWordFetch();
+    return;
+  }
+  if (event.target.dataset.action === 'showPrevPages') {
+    console.log()
+    apiService.page -= 5; 
+    makePopularOrKeyWordFetch();
+    return;
+  }
+  const currentPage = event.target;
+  apiService.page = currentPage.innerHTML;
   makePopularOrKeyWordFetch();
-  // event.target.parentElement.classList.add('--current');
-  // let prevPage = event.target.parentElement.classList.add('--current');
-  // let nextPage =  prevPage.
-  // // if(event.currentTarget.children)
-
-  // console.dir(event.target.parentElement.classList)
-  // console.dir(event.currentTarget.children)
 }
 
 function onNextPage() {
+  if (apiService.page === apiService.maxPage) {
+    return;
+  }
   apiService.page += 1;
-  makePopularOrKeyWordFetch()
+  makePopularOrKeyWordFetch();
 }
 
 function onPrevPage() {
+  if (apiService.page === 1) {
+    return;
+  }
   apiService.page -= 1;
   makePopularOrKeyWordFetch()
 };
@@ -112,5 +163,14 @@ function cleanMarkup() {
 };
 function cleanPagesMarkup() {
   refs.paginationList.innerHTML = '';
+}
+
+function highlightCurrentPage() {
+  const paginationListArray = refs.paginationList.children;
+  paginationListArray.forEach(item => {
+    if (+item.textContent === apiService.page) {
+      item.classList.add('current')
+    }
+  });
 }
 
